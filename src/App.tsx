c02ef47420f6ac6cf67329a29d2c88765ada7076
ijvazquez20 <104858329+ijvazquez20@users.noreply.github.com>
2026-04-15 20:05:06 -0300

    Lesson02

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,26 @@
 import { useState } from "react";
+import CardTile from "./components/CardTile";
 
 function App() {
   const [searchText, setSearchText] = useState("");
+  const [card, setCard] = useState(null);
+  const [errorMessage, setErrorMessage] = useState("");
 
   async function handleSearch() {
-    console.log("Searching for:", searchText);
+    setErrorMessage("");
+
+    const response = await fetch(
+      `https://api.scryfall.com/cards/named?exact=${searchText}`
+    );
+
+    if (!response.ok) {
+      setErrorMessage("Card not found. Please try another name.");
+      return;
+    }
+
+    const data = await response.json();
+    setCard(data);
+    console.log(data);
   }
 
   return (
@@ -16,6 +32,9 @@ function App() {
         placeholder="Type a card name..."
       />
       <button onClick={handleSearch}>Search</button>
+
+      {errorMessage && <p style={{ color: "red" }}>{errorMessage}</p>}
+      {card && <CardTile card={card} />}
     </div>
   );
 }
--- src/components/CardTile.tsx
+++ src/components/CardTile.tsx
@@ -6,6 +6,9 @@ interface Card {
   oracle_text: string;
   rarity: string;
   set_name: string;
+  image_uris: {
+    normal: string;
+  };
 }
 
 // "props" are how you pass data INTO a component
@@ -17,6 +20,7 @@ interface CardTileProps {
 function CardTile({ card }: CardTileProps) {
   return (
     <div style={{ border: "1px solid #ccc", padding: "16px", width: "300px", borderRadius: "8px" }}>
+      <img src={card.image_uris.normal} alt={card.name} style={{ width: "100%", borderRadius: "14px"}} />
       <h2>{card.name}</h2>
       <p><strong>Mana Cost:</strong> {card.mana_cost}</p>
       <p><strong>Type:</strong> {card.type_line}</p>
